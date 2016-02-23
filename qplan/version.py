06a3e7af60cb23c27065a5a4a7ace8de39c653e8
Eric Jeschke <eric@naoj.org>
2016-02-23 10:52:11 -1000

    Reorganize into an installable python package

---
+++ qplan/version.py
@@ -0,0 +1,7 @@
+# this file was automatically generated
+major = 0
+minor = 2
+release = 20160223204316
+
+version = '%d.%d.%d' % (major, minor, release)
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+#
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from ginga.version import version
+import os
+
+srcdir = os.path.dirname(__file__)
+
+from distutils.command.build_py import build_py
+
+setup(
+    name = "qplan",
+    version = version,
+    author = "Software Division, OCS Team",
+    author_email = "ocs@naoj.org",
+    description = ("Queue Observation Planner for Subaru Telescope."),
+    long_description = 'README.txt',
+    license = "BSD",
+    keywords = "astronomy queue planner subaru telescope",
+    url = "http://naojsoft.github.com/qplan",
+    packages = ['qplan',
+                # Misc
+                'qplan.plots', 'qplan.plugins',
+                ],
+    package_data = { 'qplan.doc': ['manual/*.rst'],
+                     },
+    scripts = ['scripts/qplan', 'scripts/qexec.py'],
+    install_requires = ['pandas>=0.13.1', 'ginga>=2.5',
+                        'matplotlib>=1.3.1'],
+    #test_suite = "",
+    classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics',
+          ],
+    cmdclass={'build_py': build_py}
+)
