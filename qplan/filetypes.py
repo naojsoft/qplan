60ff2486d7aa7cb7a6478e9f634406cdf2fa2bb5
Russell Kackley <rkackley@naoj.org>
2019-06-12 13:44:39 -1000

    Simplify the regular expression that checks the proposal ID
    	modified:   qplan/filetypes.py

--- qplan/filetypes.py
+++ qplan/filetypes.py
@@ -724,7 +724,7 @@ class WeightsFile(QueueFile):
 
 class ProposalFile(QueueFile):
 
-    propID_re = re.compile('S\d{2}[AB]-((\d{3})|EN\d{2}|(SV|TE|QN|QF|QI|OT|TEK|TEG|TEU)\d{2,3}|TE-U\d{2,3}|UH\w{1,8})$')
+    propID_re = re.compile('^S\d{2}[AB]-[-\w]{1,10}$')
 
     def __init__(self, input_dir, logger, file_ext=None):
 
