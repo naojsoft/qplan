f1b4b08de22ef2b236218695c37c410043667a79
Eric Jeschke <eric@naoj.org>
2023-08-30 17:19:25 -1000

    Add function get_datetimes_by_semester()

--- qplan/util/dates.py
+++ qplan/util/dates.py
@@ -1,4 +1,4 @@
-from dateutil import tz
+from dateutil import tz, parser
 from datetime import timedelta
 
 
@@ -37,3 +37,34 @@ def get_semester_by_datetime(dt, tz_local):
     sem = 'A' if mon in (2, 3, 4, 5, 6, 7) else 'B'
     yr  = str(year)[-2:]
     return f"S{yr}{sem}"
+
+def get_datetimes_by_semester(sem, tz_local):
+    """Figure out the beginning and ending datetimes for a semester.
+
+    Parameters
+    ----------
+    sem : str
+        A string of the form "S{YY}[AB]"
+
+    tz_local : dateutil.tz.tz.tzfile
+        The local time zone for observation
+
+    Returns
+    -------
+    (dt_start, dt_stop) : tuple of datetime.datetime
+    """
+    sem = sem.upper()
+    year = 2000 + int(sem[1:3])
+    if sem.endswith('A'):
+        dt_start = parser.parse(f"{year:04d}-02-01 12:00:00")
+        dt_stop = parser.parse(f"{year:04d}-08-01 11:59:59")
+    else:
+        dt_start = parser.parse(f"{year:04d}-08-01 12:00:00")
+        year += 1
+        dt_stop = parser.parse(f"{year:04d}-02-01 11:59:59")
+
+    # tag with timezone
+    dt_start = dt_start.replace(tzinfo=tz_local)
+    dt_stop = dt_stop.replace(tzinfo=tz_local)
+
+    return dt_start, dt_stop
