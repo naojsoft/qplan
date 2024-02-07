6bd4a5ea8a2816e696d0fd10d183dd5a8fb9ecd0
Eric Jeschke <eric@naoj.org>
2024-02-06 16:13:35 -1000

    Fix so that tomllib is used for python >= 3.11
    - updated requirements

--- qplan/cfg/HSC_cfg.py
+++ qplan/cfg/HSC_cfg.py
@@ -3,13 +3,17 @@
 #  E. Jeschke
 #
 import pathlib
-import tomli
+try:
+    import tomllib
+except ImportError:
+    # see setup.cfg, for python < 3.11
+    import tomli as tomllib
 
 from qplan.util.paths import qplan_home
 
 path = pathlib.Path(qplan_home) / 'HSC_cfg.toml'
 
 with path.open(mode="rb") as cfg_f:
-    dct = tomli.load(cfg_f)
+    dct = tomllib.load(cfg_f)
     # NOTE: defines all_filters and semester_filters
     globals().update(dct)
